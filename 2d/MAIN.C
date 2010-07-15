d33d3baff7a4371e54ccd85bbb0b6371b128dd28
AntChe <AntChe@e24598d6-fd7e-0b58-fc5e-eb1c29c93119>
2010-07-15 20:56:50 +0000

    2d (2002.12.16)

--- 2d/MAIN.C
+++ 2d/MAIN.C
@@ -1,25 +1,26 @@
 #define LEVEL
 
+//#include <conio.h>
 #include "head.h"
 
+#define EACH 100
 int main(int argc, char** argv)
 {
    double t_cur, Ttot, dttry, dtdid, dtnext;
-   int i,j,k,l, count;
-   double temp, mf, mda, mdr, en, div, see[10][10];
+   int i,k,l;
 
    Re=10000;
-   Gamma=1e-1;
+   Gamma=1e-3;
    l1=3;
    l2=1;
    l3=1;
 
    nvar=4;
-   n1=30;
+   n1=10;
    n2=1;
-   n3=30;
-   approx=7;
-   ghost=(approx-1)/2;
+   n3=10;
+   approx=7;                      //derivatives approximation order
+   ghost=(approx-1)/2;            //radius of approx sample
    dx[0]=l1/n1;
    dx[1]=l2/n2;
    dx[2]=l3/n3;
@@ -33,8 +34,10 @@ int main(int argc, char** argv)
    mm2 = ghost+n2;
    mm3 = ghost+n3;
 
+   t_cur=0; Ttot=1000;
+   count=0; enter = 0;
 
-   f  =alloc_mem_4f(nvar, m1, m2, m3);
+   f  =alloc_mem_4f(nvar, m1, m2, m3);   //f[3]-pressure,f[0,2]-v(vector)
    f1 =alloc_mem_4f(nvar, m1, m2, m3);
    df =alloc_mem_4f(nvar, m1, m2, m3);
    df2=alloc_mem_4f(nvar, m1, m2, m3);
@@ -44,65 +47,21 @@ int main(int argc, char** argv)
    nut=alloc_mem_3f(m1, m2, m3);
 
    init_conditions(f);
-   boudary_conditions(f);
-
-         div=0;
-          for(i=ghost;i<mm1;i++)
-          for(j=ghost;j<mm2;j++)
-          for(k=ghost;k<mm3;k++) {
-           temp=0;
-           for(l=0;l<3;l++)
-            temp+=dr(f[l],i,j,k,l+1,0,dx[l],ghost, approx);
-           see[i-ghost][j-ghost]=temp;
-           if (fabs(temp)>div) div=fabs(temp);
-          }
-         printf("%e %e %d %e\n", t_cur, dtdid, count, div);
-
+   printing(f,dtdid,t_cur,count);
 
    dtnext=1e-3;
-   t_cur=0; Ttot=1000;
-   count=0;
-
+   dump(f,t_cur,count);
    while (t_cur < Ttot) {            /*----- MAIN ITERATIONS ----*/
         pde(t_cur, f, df);
         dttry=dtnext;
         timestep(f, df, t_cur, f1, dttry, &dtdid, &dtnext);
         t_cur+=dtdid;
-        if (count%20==0) {
-         clrscr();
-         div=0;
-         boudary_conditions(f1);
-          for(i=ghost;i<mm1;i++)
-          for(j=ghost;j<mm2;j++)
-          for(k=ghost;k<mm3;k++) {
-           temp=0;
-           for(l=0;l<3;l++)
-            temp+=dr(f1[l],i,j,k,l+1,0,dx[l],ghost, approx);
-//            see[i-ghost][j-ghost]=temp;
-           if (fabs(temp)>div) div=fabs(temp);
-          }
-         printf("%e %e %d %e\n", t_cur, dtdid, count, div);
-        for(l=0;l<nvar;l++) {
-          mf=mda=mdr=en=0;
-          for(i=ghost;i<mm1;i++)
-          for(j=ghost;j<mm2;j++)
-          for(k=ghost;k<mm3;k++) {
-            en+=f1[l][i][j][k]*f1[l][i][j][k];
-            temp=fabs(f[l][i][j][k]-f1[l][i][j][k]);
-            if (temp>mda) mda=temp;
-            temp/=(f1[l][i][j][k]+1.e-100);
-            if (temp>mdr) mdr=temp;
-            if (fabs(f1[l][i][j][k])>mf) mf=fabs(f1[l][i][j][k]);
-          }
-         printf("%d %e %e %e %e\n",l, mf, mda, mdr, en);
-        }
-           for(k=0;k<m3;k++) printf("%e\n",f1[0][5][3][k]);
-        }
+        if (count%EACH==0)
+            printing(f1,dtdid,t_cur,count);
         for(l=0;l<nvar;l++)
         for(i=ghost;i<mm1;i++)
-        for(j=ghost;j<mm2;j++)
         for(k=ghost;k<mm3;k++)
-           f[l][i][j][k]=f1[l][i][j][k];
+           f[l][i][0][k]=f1[l][i][0][k];
 
         count++;
    }
