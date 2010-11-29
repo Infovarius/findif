347a5d547be715b767cc8e80045fba0adea55075
AntChe <AntChe@e24598d6-fd7e-0b58-fc5e-eb1c29c93119>
2010-11-29 08:15:53 +0000

    обновление до последней версии

---
+++ trunk/2d/init_vars.h
@@ -0,0 +1,15 @@
+NameMessageFile = "message.dat";
+NameErrorFile = "error.dat";
+NameNuFile = "nut.dat";
+NameVFile  = "vv.dat";
+NameDumpFile = "dump.dat";
+NameEnergyFile = "energy.dat";
+KaskadVarFile = "kaskvar.dat";
+
+//Noise=0.1;
+//NoiseNorm=0.;
+maschtab=1000000;
+
+max_okr = 3;
+lambda = 2.0;
+
