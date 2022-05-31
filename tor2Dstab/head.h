5323fbc8097e4bee3e67cf46b553abfc03c882f0
infovarius <AntChe@gmail.com>
2022-05-31 20:02:47 +0500

    добавил двумерные возмущения

---
+++ tor2Dstab/head.h
@@ -0,0 +1,137 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES	 
+//#define UNIX
+#include <math.h>
+#include <string.h>
+//#include <conio.h>
+#include <mpi.h>
+
+// #define M_PI       3.14159265358979323846L
+
+#define for2D(l)  ,l==1?l++:l
+
+LEVEL char *NameMessageFile;
+LEVEL char *NameErrorFile;
+LEVEL char NameInitFile[200];
+LEVEL char *NameVFile;
+LEVEL char *NameDumpFile;
+LEVEL char *NameSnapFile;
+LEVEL char *NameEnergyFile;
+LEVEL char *KaskadVarFile;
+LEVEL char *NameCPFile;
+LEVEL char *NameStatFile;
+LEVEL char *fname;
+
+LEVEL double ***f, ***F, ***f1, **nut, ***df, ***df2, ***df3, ***df4, ***df5,
+             dx[3], *r_1, *r_2, **refr, **refz, **sinth, **costh, **chi;
+LEVEL int **node;
+
+enum  TypeNodes {NodeUnknown=0,NodeClued=1,
+                               NodeFluid=8, NodeShell=2, NodeVacuum=4,   // regions
+                               /*NodeFluid=8,*/ NodeGhostFluid=16};       // for hydrodynamics
+LEVEL double **averf;
+LEVEL double *totvfi, *vfi;
+LEVEL double Gamma, lfi, R, rc, Re, p1;
+
+LEVEL int    m1, m3, mm1, mm3, n1, n3, N1, N3,
+             approx, ghost, nvar;
+
+LEVEL double Noise, NoiseNorm, parabole;
+LEVEL double chimax;           //maximal angle of blade tilt
+LEVEL double maschtab;
+
+LEVEL double t_cur,Ttot;
+LEVEL long count;
+LEVEL double time_begin,time_now;
+
+LEVEL long enter;
+LEVEL char razlet;
+LEVEL double TotalEnergy, PulsEnergy;
+
+LEVEL int goon;                       // continue calculation
+LEVEL int CheckStep;  //each CheckStep divergence of program is checking
+LEVEL int OutStep;    //each OutStep result is printing
+LEVEL int SnapStep;   //each SnapStep snapshot is done
+LEVEL double SnapDelta;  //each SnapDelta of simulation time snapshot is done
+LEVEL double DumpInterval;  // each DumpInterval all essential data is saved
+LEVEL int DumpKeep;   // if to keep all Dumps (0 - only 2 last)
+LEVEL int VarStep;    //each VarStep step in RungeKutta varies
+LEVEL double ChangeParamTime, DeltaParam;  // for iteration on parameters
+LEVEL int ENDPARAM;
+LEVEL long numlog;
+
+#ifndef max
+#define max(a, b)  (((a) > (b)) ? (a) : (b))
+#define min(a, b)  (((a) < (b)) ? (a) : (b))
+#endif
+#if !defined(min_d)
+#define min_d min(min(dx[0],dx[1]),dx[2])
+#endif
+/*----------------parallel---------------*/
+#if !defined(Master)
+#define Master if(rank==0)
+#endif
+LEVEL int rank, size;
+LEVEL MPI_Request SendRequest[256],RecvRequest[256];
+LEVEL MPI_Status statuses[256];
+LEVEL double *buf_send[6], *buf_recv[6];                 //6=2*3(D)
+LEVEL int pr_neighbour[6],pp[3],n[3], buf_size[3];       //3D
+LEVEL int pr[3];                                         //3D
+//void init_parallel();                       in file pde.c
+
+//--------for GOY
+LEVEL int max_okr;  //должно быть <=ghost, иначе окрестность выходит из фикт.обл-ти - виснет
+/*------- util.c  -----------*/
+void nrerror(char *mess,double t_cur,long count);
+void nmessage(char *mess,double t_cur,long count);
+void add_control_point(char *);
+double ****alloc_mem_4f(int, int, int, int);
+void   free_mem_4f(double ****, int, int, int, int);
+double ***alloc_mem_3f(int, int, int);
+void   free_mem_3f(double ***, int, int, int);
+double **alloc_mem_2f(int, int);
+void   free_mem_2f(double **, int, int);
+double *alloc_mem_1f(int);
+void   free_mem_1f(double *, int);
+int **alloc_mem_2i(int, int);
+void   free_mem_2i(int **, int, int);
+int *alloc_mem_1i(int);
+void   free_mem_1i(int *, int);
+void operate_memory(int dir);
+int isType(int nod, enum TypeNodes tip);
+void setType(int *nod, enum TypeNodes tip);
+void CopyBufferToGrid(double ***,double **,double *,int,int,int,int);
+void CopyGridToBuffer(double ***,double **,double *,int,int,int,int);
+/*------- rkad.c  -----------*/
+void timestep(double ***f, double ***df, double **nu, double t, double ***fout,
+              double dttry, double *dtdid, double *dtnext);
+double rkck(double ***f, double ***df, double **nut, double t, double dt, double ***fout);
+/*------- pde.c  -----------*/
+void   pde(double t, double ***f, double ***df);
+double norma(double a,double b,double c,int q);
+double deviation(double ***f,int,int);
+void nut_by_flux(double ***f, double **nu, double Re);
+void   boundary_conditions(double ***f, double **nu);
+void  init_conditions(void);
+void init_parallel(void);
+double dr(double **m,int ii,int kk,int dir,int or,double dx,int sh,int sm);
+double coordin(int, int);
+double interpolation(double,double, double,double,double,double);
+/*------- ioutil.c  ------*/
+FILE *fileopen(const char *, int mode);
+void fileclose(FILE *fd);
+void init_param(int argc, char** argv,double *dtnext,int flag);
+void read_params(int argc, char** argv, long count);
+int init_data(void);
+int read_snap(char* snapname);
+void print_array1d(FILE *ff,double *a,int beg1,int n1);
+void print_array2d(FILE *ff,double **a,int beg1,int n1,int beg2,int n2);
+void print_array2i(FILE *ff,int **a,int beg1,int n1,int beg2,int n2);
+void print_array3d(FILE *ff,double ***a,int beg1,int n1,int beg2,int n2,int beg3,int n3);
+void check(double ***f);
+void printing(double ***f1,double dtdid,double t_cur,long count,double en);
+void dump(double ***f1,double **nu,double t_cur,long count);
+void snapshot(double ***f,double **nu,double t_cur,long count);
+void putlog(char msg_text[],long num);
---
+++ tor2Dstab/main.c
@@ -0,0 +1,203 @@
+#define LEVEL
+
+#include "head.h"
+
+int main(int argc, char** argv)
+{
+   double dttry, dtdid, dtnext, Re_tmp, rc_tmp, tmpT, tmpPar, tmpTtot, time_old;
+   char str[200];
+   int i,tmpC;
+   int outed = 0;
+   FILE *fd=0, *ferror=0;
+   int strl;
+   double ***ft;
+
+ /* Initialize MPI */
+ MPI_Init(&argc,&argv);
+ /*  Get rank of this process and process group size */
+ MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+ MPI_Comm_size(MPI_COMM_WORLD,&size);
+
+  srand(rank);
+  NameMessageFile = "message.dat";
+  NameErrorFile = "error.err";
+  NameVFile  = "vv.dat";
+  NameEnergyFile = "energy.dat";
+  KaskadVarFile = "kaskvar.dat";
+  fname = (argc>1)? argv[1] : argv[0];
+  NameSnapFile = fname;
+  strl = (int)strlen(fname);
+  NameCPFile = (char *)calloc(strl+4,sizeof(char));
+  sprintf(NameCPFile,"%s.cp",fname);   NameCPFile[strl+3] = 0;
+  NameDumpFile =(char *)calloc(strl+5,sizeof(char));
+  sprintf(NameDumpFile ,"%s.dmp",fname); NameDumpFile[strl+4] = 0;
+  NameStatFile =(char *)calloc(strl+9,sizeof(char));
+  sprintf(NameStatFile,"%s_%d.sta",fname,rank); NameStatFile[strl+8] = 0;
+
+   numlog = 1;
+   Master nmessage("--------------------------------------------------------------------------",-1,-1);
+
+/* ---------------------- reading files and arrays --------------------- */
+   goon = ((fd=fopen(NameCPFile,"r+"))>0);
+   if(fd==NULL) { putlog("File of cp were not opened",goon);
+                  Master if((fd=fopen(NameCPFile,"w+"))!=NULL) putlog("File cp was successfully created",1);
+                }
+           else ;//putlog("File of control points opened=",(long)fd);
+   if(goon)
+		{
+		do fscanf(fd,"%s\n",NameInitFile); while (!feof(fd));
+		goon = strcmp(NameInitFile,"END") && strlen(NameInitFile)>0;
+		fileclose(fd);
+		}
+    
+nextparam: 
+	ENDPARAM = 0;
+	init_param(argc,argv,&dtnext,0);       // initialization of parameters
+	if(!goon) read_params(argc,argv,count+1);  if(ENDPARAM) goto endparam;
+   	ghost=(approx-1)/2+3;                  //radius of approx sample
+	dx[0]=2*R/N1;
+	dx[2]=2*R/N3;
+   t_cur=0;
+   count=0; enter = 0;
+
+   if(goon) {if(init_data()) nrerror("error of reading initial arrays",-1,-1);}
+       else { init_parallel();  operate_memory(1);}
+   read_snap("pumpflow_1_Re10.snp");
+   boundary_conditions(F, nut);
+  // snapshot(F, nut, 0, 1);
+   init_conditions();
+
+//--------------------------------------
+  Master {
+      fd=fileopen("coord",0);
+      for(i=0;i<N1+2*ghost;i++) fprintf(fd,"%e ",coordin(i,0));
+      fprintf(fd,"\n");
+      for(i=0;i<N3+2*ghost;i++) fprintf(fd,"%e ",coordin(i,2));
+      fileclose(fd);
+     }
+//--------------------------------------
+ if(!goon) {
+    if(rank!=0) MPI_Recv(&tmpC,1,MPI_INT,rank-1,rank-1,MPI_COMM_WORLD,statuses);
+
+ fd=fileopen("node",rank);
+
+ Master nmessage("nodes outputting has been started",t_cur,count);
+
+	print_array2i(fd,node,0,m1,0,m3);
+	print_array2d(fd,refr,0,m1,0,m3);
+	print_array2d(fd,refz,0,m1,0,m3);
+	fileclose(fd);
+         
+ if(rank!=size-1) {MPI_Send(&rank,1,MPI_INT,rank+1,rank,MPI_COMM_WORLD); }
+             else nmessage("nodes has been dumped",t_cur,count);
+            }
+
+//--------------------------------------
+
+   boundary_conditions(f,nut);
+
+//   if(!goon)  dump(f,nut,t_cur,count);
+
+   time_begin = MPI_Wtime();
+   if(!goon) { Master nmessage("work has begun",0,0); }
+       else { Master nmessage("work continued",t_cur,count); }
+   Master ferror = fileopen(NameErrorFile,abs(goon));
+
+   if(CheckStep!=0) check(f);
+   if(!goon) if (OutStep!=0) printing(f,0,t_cur,count,PulsEnergy);   //in parallel version better not to do
+
+/*------------------------ MAIN ITERATIONS -------------------------*/
+   while ((Ttot==0 || t_cur < Ttot) && !razlet) {	
+   putlog("Here", 1);  
+
+	pde(t_cur, f, df);
+	dttry=dtnext;	
+	timestep(f, df, nut, t_cur, f1, dttry, &dtdid, &dtnext);
+//	nut_by_flux(f,nut,dtdid);
+	t_cur+=dtdid;
+	time_old = time_now;
+	count++;
+        if(t_cur >= Ttot && Ttot>0) break;
+	if (CheckStep!=0 && count%CheckStep==0)
+	    {
+	    boundary_conditions(f1,nut);
+	    check(f1);
+	    }
+	if (OutStep!=0 && count%OutStep==0)
+	    {
+	    if (CheckStep!=0 && count%CheckStep!=0)
+		{
+		boundary_conditions(f1,nut);
+		check(f1);
+		}
+	      else boundary_conditions(f1,nut);
+	    printing(f1,dtdid,t_cur,count,PulsEnergy);
+	    }
+        outed = 0;
+
+	if (SnapStep!=0 && count%SnapStep==0)
+		{ snapshot(f1,nut,t_cur,count); outed=1; }
+	if (SnapDelta>5*dtdid && floor((t_cur-dtdid)/SnapDelta)<floor(t_cur/SnapDelta))
+		{ snapshot(f1,nut,t_cur,count); outed=1; }
+	ft = f;  f = f1;  f1 = ft;
+	if (count%100==0) {  // чтение параметров может перезатереть считанные
+//			MPI_Barrier(MPI_COMM_WORLD);
+			Re_tmp=Re;
+			rc_tmp = rc;	tmpC = count;  tmpT = t_cur;	tmpPar = parabole; tmpTtot = Ttot;
+			init_param(argc,argv,&dttry,0);
+			Re=Re_tmp;
+			p1 = 4./Re;
+			count = tmpC;  t_cur = tmpT;  Re = Re_tmp; rc = rc_tmp;	parabole = tmpPar; Ttot = tmpTtot;
+//			MPI_Barrier(MPI_COMM_WORLD);
+			}
+
+	if (ChangeParamTime!=0 && floor((t_cur-dtdid)/ChangeParamTime)<floor(t_cur/ChangeParamTime))
+            {
+			//Rm = floor(t_cur/ChangeParamTime+0.5)*DeltaParam-190;
+			if(!outed) { snapshot(f,nut,t_cur,count); outed = 1;}
+//                MPI_Barrier(MPI_COMM_WORLD);
+			Re_tmp = Re += DeltaParam;
+			rc_tmp = rc;	tmpC = count;  tmpT = t_cur;	tmpPar = parabole; tmpTtot = Ttot;
+			init_param(argc,argv,&dtnext,1);       // initialization of parameters
+
+			if(goon) {if(init_data()) nrerror("error of reading initial arrays",-1,-1);}
+			if(strcmp(NameInitFile,"-1")==0) goon = 1;
+
+			count = tmpC;  t_cur = tmpT;  Re = Re_tmp; rc = rc_tmp;	parabole = tmpPar; Ttot = tmpTtot;
+			init_conditions();
+			p1 = 4/Re;
+			goon = 1;
+		Master nmessage("Re was changed to",Re,count);
+            }
+	if (OutStep==0 || count%OutStep!=0) time_now=MPI_Wtime();
+	Master tmpC = DumpInterval>0 && floor((time_now-time_begin)/60/DumpInterval)>floor((time_old-time_begin)/60/DumpInterval);
+	MPI_Bcast(&tmpC,1,MPI_INT,0,MPI_COMM_WORLD);
+	if(tmpC) dump(f,nut,t_cur,count);
+
+	} // end while
+
+	printing(f1,dtdid,t_cur,count,PulsEnergy);
+	if(!outed) snapshot(f,nut,t_cur,count);
+	if(rank==0) add_control_point("END");
+
+sprintf(str,"energy(kappa=%0.2f,Re=%0.2f).dat",rc,Re);
+MPI_Barrier(MPI_COMM_WORLD);
+Master if(rename("energy.dat",str)) nmessage("Renaming of energy.dat was failed",t_cur,count);
+	operate_memory(-1);
+if(!ENDPARAM) 
+	{
+	Master nmessage("--------------------------------------------------------------------------",-1,-1);
+	goon=0;
+	goto nextparam;
+}
+endparam:
+putlog("I've got here=",4);
+	Master fileclose(ferror);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(!ENDPARAM&&t_cur>=Ttot&&!razlet) nmessage("work is succesfully done",t_cur,count);
+		else nrerror("this is break of scheme",t_cur,count);
+	MPI_Finalize();
+	nmessage("mpi_finalize is done",t_cur,count);
+return 0;
+}
